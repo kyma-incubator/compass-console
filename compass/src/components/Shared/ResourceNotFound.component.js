60bd34d7031de009cbcb3a59192b56042cc78fd9
Alex Targov <alextargov@gmail.com>
2021-01-27 17:19:03 +0200

    Fix react/no-unescaped-entities error

--- compass/src/components/Shared/ResourceNotFound.component.js
+++ compass/src/components/Shared/ResourceNotFound.component.js
@@ -33,7 +33,7 @@ const ResourceNotFound = ({
     </header>
     <Panel className="fd-has-margin-large">
       <PanelBody className="fd-has-text-align-center fd-has-type-4">
-        Such {resource} doesn't exists for this Tenant.
+        Such {resource} doesn&rsquo;t exists for this Tenant.
       </PanelBody>
     </Panel>
   </>
