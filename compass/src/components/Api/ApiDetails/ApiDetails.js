13127e57cd5e356902b15d858af66b2f407705db
Desislava Asenova <desislava.asenova@sap.com>
2021-03-30 19:38:57 +0300

    Fix linter

--- compass/src/components/Api/ApiDetails/ApiDetails.js
+++ compass/src/components/Api/ApiDetails/ApiDetails.js
@@ -151,10 +151,10 @@ const cleanEmpty = (obj) => {
   if (Array.isArray(obj)) {
     return obj
       .map((v) => (v && typeof v === 'object' ? cleanEmpty(v) : v))
-      .filter((v) => !(v == null));
+      .filter((v) => !(v === null));
   } else {
     return Object.entries(obj)
       .map(([k, v]) => [k, v && typeof v === 'object' ? cleanEmpty(v) : v])
-      .reduce((a, [k, v]) => (v == null ? a : ((a[k] = v), a)), {});
+      .reduce((a, [k, v]) => (v === null ? a : ((a[k] = v), a)), {});
   }
 };
