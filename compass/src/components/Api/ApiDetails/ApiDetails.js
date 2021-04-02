536b1d14dfc6974c26269e3a7fc993abb8cd22d7
Desislava Asenova <9128192+desislavaa@users.noreply.github.com>
2021-04-02 11:49:31 +0300

    Fix asyncapi convertion (#24)
    * Fix WS-2020-0218 vulnerability  (#14)
    
    * Bump merge dependecy version
    
    * Update generic-documentation
    
    Co-authored-by: krasish <krasishontov@gmail.com>
    
    * Clean up empty spec fields
    
    * Fix cleanup
    
    * Fix linter
    
    Co-authored-by: krasish <krasishontov@gmail.com>

--- compass/src/components/Api/ApiDetails/ApiDetails.js
+++ compass/src/components/Api/ApiDetails/ApiDetails.js
@@ -108,6 +108,8 @@ const ApiDetails = ({ apiId, eventApiId, applicationId, apiPackageId }) => {
     try {
       const parsedSpec = JSON.parse(specToShow);
       if (parsedSpec.asyncapi && parsedSpec.asyncapi.startsWith('1.')) {
+        const cleanedSpec = cleanEmpty(parsedSpec);
+        specToShow = JSON.stringify(cleanedSpec);
         specToShow = convert(specToShow, '2.0.0');
       }
     } catch (e) {
@@ -144,3 +146,15 @@ ApiDetails.propTypes = {
 };
 
 export default ApiDetails;
+
+const cleanEmpty = (obj) => {
+  if (Array.isArray(obj)) {
+    return obj
+      .map((v) => (v && typeof v === 'object' ? cleanEmpty(v) : v))
+      .filter((v) => !(v === null));
+  } else {
+    return Object.entries(obj)
+      .map(([k, v]) => [k, v && typeof v === 'object' ? cleanEmpty(v) : v])
+      .reduce((a, [k, v]) => (v === null ? a : ((a[k] = v), a)), {});
+  }
+};
