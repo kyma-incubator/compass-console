ceb30356fb5939029086f52f820de3522739951f
Alex Targov <alextargov@gmail.com>
2021-10-14 15:24:43 +0300

    Add missing statuses

--- compass/src/components/Shared/getBadgeTypeForStatus.js
+++ compass/src/components/Shared/getBadgeTypeForStatus.js
@@ -8,7 +8,16 @@ export const getBadgeTypeForStatus = (status) => {
       return 'info';
     case 'CONNECTED':
       return 'success';
+    case 'UNPAIRING':
+    case 'CREATING':
+    case 'DELETING':
+    case 'UPDATING':
+      return 'warning';
     case 'FAILED':
+    case 'DELETE_FAILED':
+    case 'UNPAIR_FAILED':
+    case 'CREATE_FAILED':
+    case 'UPDATE_FAILED':
       return 'error';
     default:
       return undefined;
