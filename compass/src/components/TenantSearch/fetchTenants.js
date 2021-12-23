41669bc2e276162ec94cf7cefb8ff37b889417bf
Alex Targov <alextargov@gmail.com>
2021-12-23 10:38:58 +0200

    Add paging to tenant search

--- compass/src/components/TenantSearch/fetchTenants.js
+++ compass/src/components/TenantSearch/fetchTenants.js
@@ -1,13 +1,37 @@
-export default async function fetchTenants(token, apiUrl) {
+export default async function fetchTenants(
+  token,
+  apiUrl,
+  searchTerm = '',
+  pageSize,
+  endCursor,
+) {
+  const variables =
+    pageSize > 0 && !!endCursor
+      ? {
+          first: pageSize,
+          after: endCursor,
+          searchTerm,
+        }
+      : {
+          searchTerm,
+          first: 5,
+        };
   const query = {
-    query: `{
-    tenants {
-      name
-      id
-      initialized
-    }
-  }
-  `,
+    query: `query tenants($first:Int, $after: PageCursor, $searchTerm: String) {
+        tenants(first: $first, after:$after, searchTerm:$searchTerm) {
+          data {
+            id
+            name
+          }
+          pageInfo {
+            startCursor
+            hasNextPage
+            endCursor
+          }
+          totalCount
+        }
+      }`,
+    variables,
   };
 
   const response = await fetch(apiUrl, {
