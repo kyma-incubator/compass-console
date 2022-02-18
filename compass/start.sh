325949fdfe0b9e749113fcaf0802a0cb4a67c05d
krasish <krasishontov@gmail.com>
2022-02-18 15:13:01 +0200

    Fix dockerfile

--- compass/start.sh
+++ compass/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 NGINX_CONFIG="$1"
-sed -i "" "s,\$allowedConnectSrc,$ALLOWED_CONNECT_SRC,g" $NGINX_CONFIG
-nginx -g daemon off;
+sed -i "s,\$allowedConnectSrc,$ALLOWED_CONNECT_SRC,g" "$NGINX_CONFIG"
+nginx -g "daemon off;"
