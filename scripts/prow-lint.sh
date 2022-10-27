1a9e2e2f2ac2cd016f5464526963581a0f4fb793
i322062 <kaloyan.spiridonov@sap.com>
2022-10-27 12:47:48 +0300

    add prow lint and test scripts

---
+++ scripts/prow-lint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+npm install eslint-plugin-jsdoc@15.12.2
+npm run lint-check
---
+++ scripts/prow-test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+npm install jest@26.6.3
+npm install react-tippy@1.4.0
+npm install jsoneditor@9.6.0
+npm run test-shared-lib
